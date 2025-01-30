c291479ee824dd360f8fd80300fbb1db77ed17a2
Vipul Singh <singhvipul@microsoft.com>
2025-01-30 13:02:40 +0000

    Cli connectivity test for SDP

---
+++ cilium-cli/connectivity/builder/check_standalone_dns_proxy.go
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package builder
+
+import (
+	"github.com/cilium/cilium/cilium-cli/connectivity/check"
+	"github.com/cilium/cilium/cilium-cli/connectivity/tests"
+	"github.com/cilium/cilium/cilium-cli/utils/features"
+)
+
+type checkStandaloneDnsProxy struct{}
+
+func (t checkStandaloneDnsProxy) build(ct *check.ConnectivityTest, templates map[string]string) {
+	newTest("check-standalone-dns-proxy", ct).
+		WithCiliumPolicy(templates["clientEgressOnlyDNSPolicyYAML"]).
+		WithFeatureRequirements(features.RequireEnabled(features.L7Proxy), features.RequireEnabled(features.IPv4)). // Flags for standalone DNS proxy
+		WithScenarios(
+			tests.StandaloneDNSProxy(),
+		)
+}
